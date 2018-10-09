87f0a5f0b73438e58a8a1b2a5ce3ec7317187af8
terecrem <teresacremo@hotmail.it>
2018-10-09 23:04:01 +0200

    cremonesi

--- sketch.js
+++ sketch.js
@@ -1,11 +1,41 @@
-function preload(){
-  // put preload code here
+function setup() {
+  createCanvas(800, 600);
+  noLoop();
 }
 
-function setup() {
-  // put setup code here
-}
+
+var colorList = ['#59BEBD',
+  '#924C69',
+  '#EE7D98',
+  '#16404F'
+]
 
 function draw() {
-  // put drawing code here
+
+
+  background(0);
+
+
+  for (var x = 0; x <= width; x += 50) {
+    for (var y = 0; y <= height; y += 50) {
+
+      fill(random(colorList));
+
+      if (y % (50 * 2) == 0) {
+        ellipse(x, y, 30, 30);
+      } else {
+        ellipse(x + 50 / 2, y, 40, 40);
+      }
+    }
+  }
+
+  fill(255,255,255);
+textSize(40);
+textFont('Andale Mono')
+text("/ Click to change color",85,560);
+noLoop();
+}
+
+function mousePressed() {
+  redraw();
 }
